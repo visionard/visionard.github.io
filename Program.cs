4d91fd35ec1dc881c32ebac110c57847127134b3
Avinash Seth <bloggerbeg@gmail.com>
2019-02-01 11:14:34 +0530

    Update Program.cs
    added comments

--- Program.cs
+++ Program.cs
@@ -15,15 +15,15 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Creating Virtual Machine");
             var credentials = SdkContext.AzureCredentialsFactory
-                .FromFile("local_path_location");
+                .FromFile("local_path_location"); // azureauth.properties file location
             var azure = Azure
                 .Configure()
                 .WithLogLevel(HttpLoggingDelegatingHandler.Level.Basic)
                 .Authenticate(credentials)
                 .WithDefaultSubscription();
             var groupName = "myResourceGroup";
-            var templatePath = "location";
-            var paramPath = "location";
+            var templatePath = "location"; // MyVM.json file location which should be like http://validurl.com/path/to/MyVM.json
+            var paramPath = "location"; // Parameters.json file lcoation which should be like https://validurl.com/path/to/Paramters.json
             var deployment = azure.Deployments.Define("deploymentName")
                 .WithExistingResourceGroup(groupName)
                 .WithTemplateLink(templatePath, "1.0.0.0")
